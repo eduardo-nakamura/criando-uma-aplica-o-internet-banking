6a3815d7cc5ee4cacc7e84e062266566fc8bdbbf
eduardo-nakamura <edu-nakamura@hotmail.com>
2019-08-26 10:33:38 -0300

    Cadastro de usuários no Banco

---
+++ traning-bank/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component'
+
+const routes: Routes = [
+  { path: '', component: ContentComponent },
+  { path: 'cadastro', component: ContentComponent }
+  ];
+  
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
